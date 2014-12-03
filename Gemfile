f52a717190a5fdb94304d6031093477b27a42833
carolvisoto <carolvisoto@gmail.com>
2014-12-03 15:39:15 +0100

    update heroku with bootstrap

--- Gemfile
+++ Gemfile
@@ -1,5 +1,10 @@
 
 source 'http://rubygems.org'
+
 gem 'sinatra'
 gem 'thin'
 gem 'soundcloud'
+gem 'activerecord'
+gem "sinatra-activerecord"
+gem "sqlite3"
+gem "rake"
--- server.rb
+++ server.rb
@@ -1,11 +1,10 @@
 require 'sinatra'
 require 'soundcloud'
 
-set :server, %w[thin webrick]
-
 get '/' do
-  'Hello world!'
+  erb :hello
 end
+
 
 get '/home/:name' do
   "Hello here is  #{params['name']}"
@@ -18,5 +17,25 @@ end
 get '/tracks/:id' do
    client = SoundCloud.new(:client_id => '69e93cf2209402f6f3137a6452cf498f')
    @track = client.get("/tracks/#{params[:id]}")
+    
+    pic = @track.artwork_url
+    if pic.nil?
+      pic = @track.user[:avatar_url]    
+    end
+    @image ="#{pic}".sub("large", "crop")
+
    erb :tracks
+end
+
+get '/playlists/:id' do
+   client = SoundCloud.new(:client_id => '69e93cf2209402f6f3137a6452cf498f')
+   @playlist = client.get("/playlists/#{params[:id]}")
+
+    pic = @playlist.artwork_url
+    if pic.nil?
+      pic = @playlist.user[:avatar_url]    
+    end
+    @image ="#{pic}".sub("large", "crop")
+    
+   erb :playlists
 end
